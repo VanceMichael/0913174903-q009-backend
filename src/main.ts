1f4ef3da9fd63b2ace680dc50fa3a13f3c814b4d
Ubuntu <ubuntu@localhost.localdomain>
2026-09-13 18:22:19 +0800

    收敛服务适配器依赖

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module.ts";
 
-const app = await NestFactory.create(AppModule);
+const app = await NestFactory.create(AppModule, new FastifyAdapter());
 await app.listen(Number(process.env.PORT ?? "8080"), "0.0.0.0");
